0d5d989433de2bf06a8275ac38a12caac0985601
Bogdan <bdinulescu85@gmail.com>
2025-10-11 02:10:32 +0100

    Add project files.

---
+++ GlobalVariables.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+public static class GlobalUserId
+{
+    public static string userId;
+}
+
+public static class ConnectionString
+{
+    public static string dbPath = Path.Combine(AppDomain.CurrentDomain.BaseDirectory, "HospitalDB.db");
+    public static string connectionString = $"Data Source={dbPath};Version=3;Foreign Keys=True;";
+}
